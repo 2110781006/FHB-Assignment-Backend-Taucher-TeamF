9babc373275820493b4d6c3a90d9e181aacb2dab
2110781006 <2110781006@fh-burgenland.at>
2022-05-30 18:51:58 +0000

    fix eslint errors

--- test/index.test.js
+++ test/index.test.js
@@ -3,14 +3,18 @@ const request = require('supertest'); //http assertion lib
 const index = require('../index');    //index js
 
 //first testcase
+// eslint-disable-next-line no-undef
 describe('Check if api url is reachable', () => {
+    // eslint-disable-next-line no-undef
     it('should return 200 (Status OK)', async () => {
         await request(index.app).get('/api/notes').expect(200);
     });
 });
 
 //second testcase
+// eslint-disable-next-line no-undef
 describe('Check if notes are returned', () => {
+    // eslint-disable-next-line no-undef
     it('count should be 4', async () => {
         var notes = await(await request(index.app).get('/api/notes')).body;
 
@@ -19,7 +23,9 @@ describe('Check if notes are returned', () => {
 });
 
 //third testcase
+// eslint-disable-next-line no-undef
 describe('Check if note with id 1 is retured', () => {
+    // eslint-disable-next-line no-undef
     it('id should be 1', async () => {
         var note = await(await request(index.app).get('/api/notes/1')).body;
         assert.equal(note['id'], 1);
@@ -27,16 +33,22 @@ describe('Check if note with id 1 is retured', () => {
 });
 
 //fourth testcase
+// eslint-disable-next-line no-undef
 describe('Check if note have all properties', () => {
+    // eslint-disable-next-line no-undef
     it('id should have id, content, date and important', async () => {
         var notes = await(await request(index.app).get('/api/notes')).body;
 
-        for(note in notes)
+        for(var note in notes)
         {
-            assert.equal(notes[note].hasOwnProperty('id'), true);
+            assert.equal(Object.prototype.hasOwnProperty.call(notes[note], "id"));
+            assert.equal(Object.prototype.hasOwnProperty.call(notes[note], "content"));
+            assert.equal(Object.prototype.hasOwnProperty.call(notes[note], "date"));
+            assert.equal(Object.prototype.hasOwnProperty.call(notes[note], "important"));
+            /*assert.equal(notes[note].hasOwnProperty('id'), true);
             assert.equal(notes[note].hasOwnProperty('content'), true);
             assert.equal(notes[note].hasOwnProperty('date'), true);
-            assert.equal(notes[note].hasOwnProperty('important'), true);
+            assert.equal(notes[note].hasOwnProperty('important'), true);*/
         }
     });
 });
